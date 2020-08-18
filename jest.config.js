ff442a94f2004912b5fb2ca4f859ab4820fd8455
Mac Button <demonzero14@gmail.com>
2020-08-18 12:56:58 -0700

    metadata-entry-form-1: Added a sample test and coverage. Couldn't use the preset in create react app do to a bug that stops rendering from working when coverage is enabled

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "collectCoverageFrom": [
+    "src/**/*.{js,jsx}",
+    "!**/node_modules/**",
+  ],
+  "testRegex": "(/__tests__/.*|\\.(test|spec))\\.(js|jsx)$",
+  "moduleFileExtensions": ["js", "json", "jsx"],
+};
