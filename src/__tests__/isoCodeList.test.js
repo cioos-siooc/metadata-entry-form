f8f332a8627719afbadb8fe78d095abfb08a4790
Mac Button <demonzero14@gmail.com>
2020-08-18 16:12:07 -0700

    metadata-entry-form-9: Add unit tests for components

---
+++ src/__tests__/isoCodeList.test.js
@@ -0,0 +1,7 @@
+import { eovList, progressCodes, roleCodes } from "../isoCodeLists";
+
+it('Defines constants', () => {
+  expect(eovList).toBeDefined();
+  expect(progressCodes).toBeDefined();
+  expect(roleCodes).toBeDefined();
+});
---
+++ src/components/__tests__/MetadataForm.test.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import MetadataForm from '../MetadataForm';
+import { Button } from '@material-ui/core';
+import BilingualTextInput from '../BilingualTextInput';
+
+configure({ adapter: new Adapter() });
+
+const mockFirebase = jest.fn()
+jest.mock("../../firebase", () => ({
+  database: () => ({ ref: () => ({ push: mockFirebase }) })
+}));
+
+describe('<MetadataForm />', () => {
+  it('Renders', () => {
+    mount(<MetadataForm />);
+  });
+
+  it('Handles button clicks', async () => {
+    const mockPush = jest.fn();
+    const wrapper = mount(<MetadataForm history={{push: mockPush}} />);
+
+    await wrapper.find(Button).props().onClick();
+    expect(mockFirebase).toHaveBeenCalled();
+    expect(mockPush).toHaveBeenCalled();
+
+  });
+
+  it('Handles input changes without a parent', () => {
+    const wrapper = mount(<MetadataForm />);
+
+    console.log(wrapper.instance());
+    wrapper.find(BilingualTextInput).at(0).props().onChange({ target: {name: "Gandalf", value: "Narya"}});
+  });
+
+  it('Handles input changes with a parent', () => {
+    const wrapper = mount(<MetadataForm />);
+
+    wrapper.find(BilingualTextInput).at(0).props().onChange({ target: {name: "Gandalf", value: "Narya"}}, true);
+  });
+});
