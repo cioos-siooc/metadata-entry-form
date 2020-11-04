33a116b050d09bfce80fcafe0debb6c81cf5138e
ogsletcax <caxarde@ogsl.ca>
2020-11-04 20:50:27 +0000

    move i18n dictionaries

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,21 +1,21 @@
-import fr from "../i18nDictionaryFr";
-import en from "../i18nDictionaryEn";
+import fr from "./i18nDictionaryFr";
+import en from "./i18nDictionaryEn";
 
 const translate = (textOrCode, locale = null, context = "") => {
-  const dictionary = locale === "fr" ? fr : en;
-  let key = textOrCode;
+    const dictionary = locale === "fr" ? fr : en;
+    let key = textOrCode;
 
-  if (context) key = `[${context}]${textOrCode}`;
+    if (context) key = `[${context}]${textOrCode}`;
 
-  if (dictionary[key]) {
-    return dictionary[key];
-  }
+    if (dictionary[key]) {
+        return dictionary[key];
+    }
 
-  if (dictionary[textOrCode]) {
-    return dictionary[textOrCode];
-  }
+    if (dictionary[textOrCode]) {
+        return dictionary[textOrCode];
+    }
 
-  return textOrCode;
+    return textOrCode;
 };
 
 export default translate;
