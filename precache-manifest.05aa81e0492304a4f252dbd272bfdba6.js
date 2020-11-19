4e2acb3cf5744c77d132303e00505f5f6caabad8
Nate <nate.rosenstock@hakai.org>
2020-11-18 22:59:30 -0800

    Updates

---
+++ precache-manifest.05aa81e0492304a4f252dbd272bfdba6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67bffa4e216945e130284e307b59d538",
+    "url": "/metadata-entry-form/index.html"
+  },
+  {
+    "revision": "ba66ee7999717113fd99",
+    "url": "/metadata-entry-form/static/css/2.91a17f1e.chunk.css"
+  },
+  {
+    "revision": "3e24706846aa2fba2c45",
+    "url": "/metadata-entry-form/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ba66ee7999717113fd99",
+    "url": "/metadata-entry-form/static/js/2.ee848e0b.chunk.js"
+  },
+  {
+    "revision": "2794ffa452bd6acd80e68d07335f8c7c",
+    "url": "/metadata-entry-form/static/js/2.ee848e0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e24706846aa2fba2c45",
+    "url": "/metadata-entry-form/static/js/main.a57d6a08.chunk.js"
+  },
+  {
+    "revision": "b85c89d0981620d2e991",
+    "url": "/metadata-entry-form/static/js/runtime-main.0b7029d3.js"
+  },
+  {
+    "revision": "fd5728f2cf777b06b966d05c0c823dc9",
+    "url": "/metadata-entry-form/static/media/spritesheet.fd5728f2.svg"
+  }
+]);
