4ae10088740626c1f9c793cd4bce725db0ca69ec
Nate <nate.rosenstock@hakai.org>
2021-01-14 12:49:11 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/metadata-entry-form/precache-manifest.739143f0d5de3098b87b41b49d505c54.js"
+  "/metadata-entry-form/precache-manifest.09e2dca6f14335cb327b2242eeeb771f.js"
 );
 
 self.addEventListener('message', (event) => {
