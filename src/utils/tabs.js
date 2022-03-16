0218e5a80bfeb3783769f8ca64164a496e51593d
Nate <nate.rosenstock@hakai.org>
2022-03-16 10:40:16 -0700

    reorder tabs on submit page

--- src/utils/tabs.js
+++ src/utils/tabs.js
@@ -2,14 +2,13 @@ const tabs = {
   // Submit section
   start: { en: "Start", fr: "Accueil" },
   dataID: { en: "Data Identification", fr: "Identification des données" },
-  resources: { en: "Resources", fr: "Ressources" },
   spatial: { en: "Spatial", fr: "Spatial" },
+  contacts: { en: "Contacts", fr: "Contacts" },
+  resources: { en: "Resources", fr: "Ressources" },
   platform: { en: "Platform", fr: "Plateforme" },
   platformInstruments: {
     en: "Platform - instruments",
     fr: "Plateforme - instruments",
   },
-
-  contacts: { en: "Contacts", fr: "Contacts" },
 };
 export default tabs;
