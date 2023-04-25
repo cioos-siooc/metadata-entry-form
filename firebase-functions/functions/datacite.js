7bb49f83a1302008d5fadebb482c5e86afd6f37a
Austen Sorochak <austensorochak@gmail.com>
2023-04-25 13:14:37 -0700

    add error handling to updateDraftDoi

--- firebase-functions/functions/datacite.js
+++ firebase-functions/functions/datacite.js
@@ -8,8 +8,8 @@ exports.createDraftDoi = functions.https.onCall(async (record) => {
 
   const response = await axios.post(url, record, {
     headers: {
-      authorization: `Basic ${DATACITE_AUTH_HASH}`,
-      content_type: "application/json",
+      'Authorization': `Basic ${DATACITE_AUTH_HASH}`,
+      'Content-Type': 'application/json',
     },
   });
 
@@ -17,22 +17,53 @@ exports.createDraftDoi = functions.https.onCall(async (record) => {
 });
 
 exports.updateDraftDoi = functions.https.onCall(async (data) => {
+  try{
+    const url = `${baseUrl}${data.doi}/`;
+    const response = await axios.put(url, data.data, {
+      headers: {
+        'Authorization': `Basic ${DATACITE_AUTH_HASH}`,
+        'Content-Type': "application/json",
+      },
+    });
+    return {
+      status: response.status,
+      message: 'Draft DOI updated successfully',
+    };
+  } catch (err) {
+    // if the error is a 401, throw a HttpsError with the code 'unauthenticated'
+    if (err.response && err.response.status === 401) {
+      throw new functions.https.HttpsError(
+        'unauthenticated',
+        'Error from DataCite API: Unauthorized. Please check your API credentials.'
+      );
+    }
+    // if the error is a 404, throw a HttpsError with the code 'unauthenticated'
+    if (err.response && err.response.status === 404) {
+      throw new functions.https.HttpsError(
+        'not-found',
+        'from DataCite API: Not-found. The resource is not found e.g. it fetching a DOI/Repository/Member details.'
+      );
+    }
+    // initialize a default error message
+    let errMessage = 'An error occurred while updating the draft DOI.';
 
+    // if there is an error response from DataCite, include the status and statusText from the API error
+    // if the error doesn't have a response, include the error message
+    if (err.response) {
+      errMessage = `from DataCite API: ${err.response.status} - ${err.response.statusText}`;
+    } else if (err.message) {
+      errMessage = err.message;
+    }
 
-  const url = `${baseUrl}${data.doi}/`;
-  const response = await axios.put(url, data.data, {
-    headers: {
-      authorization: `Basic ${DATACITE_AUTH_HASH}`,
-      content_type: "application/json",
-    },
-  });
-  return response.status;
+    // throw a default HttpsError with the code 'unknown' and the error message
+    throw new functions.https.HttpsError('unknown',errMessage);
+  }
 });
 
 exports.deleteDraftDoi = functions.https.onCall(async (draftDoi) => {
   const url = `${baseUrl}${draftDoi}/`;
   const response = await axios.delete(url, {
-    headers: { authorization: `Basic ${DATACITE_AUTH_HASH}` },
+    headers: { 'Authorization': `Basic ${DATACITE_AUTH_HASH}` },
   });
   return response.status;
 });
