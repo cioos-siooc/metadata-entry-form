ee367198430c9df8d1fd8644ff477b6377623905
Austen Sorochak <austensorochak@gmail.com>
2023-04-26 16:10:01 -0700

    pretty print the datacite json download

--- src/components/FormComponents/MetadataRecordListItem.jsx
+++ src/components/FormComponents/MetadataRecordListItem.jsx
@@ -102,7 +102,7 @@ const MetadataRecordListItem = ({
       } else if (fileType === "erddap") {
         data = [recordToERDDAP(record)];
       } else if (fileType === "json") {
-        data = [JSON.stringify(recordToDataCite(record))];
+        data = [JSON.stringify(recordToDataCite(record), null, 2)];
       } else {
         const res = await downloadRecord({ record, fileType });
         data = Object.values(res.data.message);
