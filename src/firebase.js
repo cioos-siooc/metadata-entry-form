976fc81b70ad3749cfd995325b4af90af0327414
Matthew Foster <matthew.foster@hakai.org>
2023-11-02 19:43:34 -0700

    initial attempt at firebase emulator in docker

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import "firebase/auth";
 import "firebase/functions";
 import "firebase/firestore";
 
-const deployedOnTestSserver = process.env.REACT_APP_DEV_DEPLOYMENT;
+const deployedOnTestServer = process.env.REACT_APP_DEV_DEPLOYMENT;
 
 const productionDB = "https://cioos-metadata-form.firebaseio.com";
 const devDB = "https://cioos-metadata-form-dev.firebaseio.com";
@@ -13,7 +13,7 @@ const config = {
   apiKey: "AIzaSyAdlELZS5Lbea5NquotMT8amwO-Lc_7ogc",
   authDomain: "cioos-metadata-form.firebaseapp.com",
   databaseURL:
-    process.env.NODE_ENV === "production" && !deployedOnTestSserver
+    process.env.NODE_ENV === "production" && !deployedOnTestServer
       ? productionDB
       : devDB,
   projectId: "cioos-metadata-form",
@@ -22,11 +22,15 @@ const config = {
   appId: "1:646114203434:web:bccceadc5144270f98f053",
 };
 
+if (window.location.hostname === "localhost" && deployedOnTestServer) {
+  config['databaseURL'] = "http://localhost:9001?ns=cioos-metadata-form"
+}
+
 firebase.initializeApp(config);
 // uncomment below to use firebase emulator for local development
-// if (window.location.hostname === "localhost") {
-//   firebase.functions().useFunctionsEmulator("http://localhost:5002");
-//   // firebase.auth().useEmulator("http://localhost:9099");
-// }
+if (window.location.hostname === "localhost" && deployedOnTestServer) {
+  firebase.functions().useFunctionsEmulator("http://localhost:5002");
+  // firebase.auth().useEmulator("http://localhost:9099");
+}
 
 export default firebase;
