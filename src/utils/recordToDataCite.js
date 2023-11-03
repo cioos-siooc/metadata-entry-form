5ad06f28befb28e24bda91256f33089f7727ac90
Austen Sorochak <austensorochak@gmail.com>
2023-11-03 15:31:56 -0700

    remove console logs

--- src/utils/recordToDataCite.js
+++ src/utils/recordToDataCite.js
@@ -4,8 +4,6 @@ import regions from "../regions";
 
 function recordToDataCite(metadata, language, region) {
 
-  console.log(JSON.stringify(metadata));
-
     // Reduce contacts to a list of creators
     const creators = metadata.contacts ? metadata.contacts.reduce((creatorList, contact) => {
       let creator;
@@ -215,8 +213,6 @@ function recordToDataCite(metadata, language, region) {
 
     // Generate URL element
     mappedDataCiteObject.data.attributes.url = `${regions[region].catalogueURL[language]}dataset/ca-cioos_${metadata.identifier}`;
-
-    console.log(JSON.stringify(mappedDataCiteObject))
 
     return mappedDataCiteObject;
   }
