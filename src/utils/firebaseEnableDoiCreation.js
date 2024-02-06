0a184b9cf46e7af8d7c4255d9b65e83c9efbb09a
Austen Sorochak <austensorochak@gmail.com>
2024-02-06 15:50:56 -0800

    add firebaseEnableDoiCreation functions

---
+++ src/utils/firebaseEnableDoiCreation.js
@@ -0,0 +1,27 @@
+import firebase from "../firebase";
+
+export async function newDataciteAccount(region, prefix, authHash) {
+    const dataciteRef = await firebase
+        .database()
+        .ref(region)
+        .child("dataciteCredentials");
+    
+    // Setting prefix and authHash directly under dataciteCredentials
+    const newNode = await dataciteRef.push({
+        prefix,
+        authHash,
+    });
+
+    return newNode.key;
+}
+
+export async function deleteDataciteAccount(region, credentialKey) {
+  // Reference to the specific dataciteCredentials entry using the credentialKey
+  const credentialRef = firebase.database().ref(region).child("dataciteCredentials").child(credentialKey);
+
+  // Deleting the specified entry
+  await credentialRef.remove();
+
+  // Return a message indicating success, or handle errors as needed
+  return { success: true, message: "Datacite account deleted successfully." };
+}
