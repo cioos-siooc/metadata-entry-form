f200e3d8b76605d61404b68e90b90672cbac67d4
Matthew Foster <matthew.foster@hakai.org>
2024-02-08 14:59:06 -0800

    fix tests

--- src/components/__tests__/DateInput.test.jsx
+++ src/components/__tests__/DateInput.test.jsx
@@ -6,13 +6,8 @@ import { KeyboardDatePicker } from "@material-ui/pickers";
 
 import DateInput from "../FormComponents/DateInput";
 
-// this helps mock the a date
-const mockedData = new Date("2020-11-26T00:00:00.000Z");
-jest.spyOn(global, "Date").mockImplementation(() => mockedData);
-Date.now = () => 1606348800;
-
 configure({ adapter: new Adapter() });
-const mockEventValue = new Date("2021-09-08T22:23:52.468Z");
+const mockEventValue = new Date("2021-09-08T19:00:00.000Z");
 const mockComponentName = "date";
 const mockOnChange = jest.fn();
 
@@ -22,6 +17,16 @@ jest.mock("react-router-dom", () => ({
     language: "en",
   }),
 }));
+
+
+beforeAll(() => {
+  jest.useFakeTimers('modern');
+  jest.setSystemTime(new Date("2021-09-08T19:00:00.000Z"));
+});
+
+afterAll(() => {
+  jest.useRealTimers();
+});
 
 describe("<DateInput />", () => {
   it("Updates the date when it is changed", () => {
