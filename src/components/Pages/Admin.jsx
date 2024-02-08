5898a6688c529b6864250bbe1f1deb6316a64317
Austen Sorochak <austensorochak@gmail.com>
2024-02-08 11:43:19 -0800

    return Admin to class

--- src/components/Pages/Admin.jsx
+++ src/components/Pages/Admin.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import {
   Typography,
   Button,
@@ -12,91 +12,65 @@ import firebase from "../../firebase";
 import { getRegionProjects } from "../../utils/firebaseRecordFunctions";
 import { auth } from "../../auth";
 import { En, Fr, I18n } from "../I18n";
+import FormClassTemplate from "./FormClassTemplate";
 
 import { unique } from "../../utils/misc";
 
 const cleanArr = (arr) => unique(arr.map((e) => e.trim()).filter((e) => e));
 
-const Admin = (props) => {
-  const { match } = props;
-  const { region } = match.params;
-  const mounted = useRef(false);
+class Admin extends FormClassTemplate {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // array of strings which are email addresses or reviewers, admins
+      admins: [],
+      projects: [],
+      reviewers: [],
+      loading: false,
+    };
+  }
 
-  // const [regions, setRegions] = useState([]);
-  const [state, setState] = useState({
-    admins: [],
-    projects: [],
-    reviewers: [],
-    loading: false,
-  })
+  async componentDidMount() {
+    const { match } = this.props;
+    const { region } = match.params;
 
+    this.setState({ loading: true });
 
-  // sets up auth listeners
-  useEffect(() => {
-    mounted.current = true;
-    let projects;
-
-    // Before initiating any async operations, the component's state is updated to indicate loading is in progress.
-    setState((prevState) => ({ ...prevState, loading: true }));
-
-    // subscribe to auth state changes
-    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+    this.unsubscribe = auth.onAuthStateChanged(async (user) => {
       if (user) {
         // Reference to the regionAdmin in the database
         const regionAdminRef = firebase.database().ref('admin').child(region)
-        
-        // Reference to permissions
         const permissionsRef = regionAdminRef.child('permissions');
-        // fetch projects for the region
-        if (mounted.current) {
-          projects = await getRegionProjects(region);
-        }
-        
-        // Listen for changes to permissions
-        const unsubscribePermissions = permissionsRef.on('value', (permissionsFirebase) => {
+        const projects = await getRegionProjects(region);
+        permissionsRef.on("value", (permissionsFirebase) => {
           const permissions = permissionsFirebase.toJSON();
+
           // const projects = permissions.projects.split(",");
           const admins = permissions.admins.split(",");
           const reviewers = permissions.reviewers.split(",");
 
-          // Update state with fetched data
-          setState(prevState => ({
-            ...prevState,
+          this.setState({
             projects,
             admins,
             reviewers,
             loading: false,
-          }));
+          });
         });
-
-        // Cleanup function for permissionsRef listener
-        return () => permissionsRef.off('value', unsubscribePermissions);
+        this.listenerRefs.push(permissionsRef);
       }
-    return () => {};
     });
-    // Cleanup function for the entire useEffect hook
-    // This is to clean up the auth listener when the component unmounts
-  return () => {
-      mounted.current = false;
-      unsubscribe();
-  };
-  }, [region]);
+  }
 
-  // fetch the available regions on component mount
-  // useEffect(() => {
-  //   const fetchRegions = async () => {
-  //     const regionsRef = firebase.database().ref('regions');
-  //   }
-  // })
+  save() {
+    const { match } = this.props;
+    const { region } = match.params;
 
-  const save = () => {
-
-    const { reviewers, admins, projects } = state;
+    const { reviewers, admins, projects } = this.state;
 
     if (auth.currentUser) {
-      const regionRef = firebase.database().ref(region);
-      const permissionsRef = regionRef.child("permissions");
-      const projectsRef = regionRef.child("projects");
+      const regionAdminRef = firebase.database().ref('admin').child(region)
+      const permissionsRef = regionAdminRef.child('permissions');
+      const projectsRef = regionAdminRef.child("projects");
 
       permissionsRef.child("admins").set(cleanArr(admins).join());
 
@@ -105,188 +79,108 @@ const Admin = (props) => {
     }
   }
 
-  const { loading, reviewers, admins, projects } = state;
+  render() {
+    const { loading, reviewers, admins, projects } = this.state;
+    return (
+      <Grid container direction="column" spacing={3}>
+        <Grid item xs>
+          <Typography variant="h5">
+            <I18n>
+              <En>Admin</En>
+              <Fr>Administrateurs</Fr>
+            </I18n>
+          </Typography>
+          <Typography>
+            <I18n>
+              <En>
+                Add each admin or reviewer's email address on it's own line
+              </En>
+              <Fr>
+                Ajouter l'adresse e-mail de chaque administrateur ou réviseur
+                sur sa propre ligne
+              </Fr>
+            </I18n>
+          </Typography>
+        </Grid>
 
-  return (
-    <Grid container direction="column" spacing={3}>
-      <Grid item xs>
-        <Typography variant="h5">
-          <I18n>
-            <En>Admin</En>
-            <Fr>Administrateurs</Fr>
-          </I18n>
-        </Typography>
-        <Typography>
-          <I18n>
-            <En>Add each admin or reviewer's email address on it's own line</En>
-            <Fr>
-              Ajouter l'adresse e-mail de chaque administrateur ou réviseur sur
-              sa propre ligne
-            </Fr>
-          </I18n>
-        </Typography>
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <>
+            <Grid item xs>
+              <Typography>
+                <I18n>
+                  <En>Projects</En>
+                  <Fr>Projets</Fr>
+                </I18n>
+              </Typography>
+            </Grid>
+            <Grid item xs>
+              <TextField
+                multiline
+                fullWidth
+                value={projects.join("\n")}
+                onChange={(e) =>
+                  this.setState({ projects: e.target.value.split("\n") })
+                }
+              />
+            </Grid>
+            <Grid item xs>
+              <Typography>
+                <I18n>
+                  <En>Admins</En>
+                  <Fr>Administrateurs</Fr>
+                </I18n>
+              </Typography>
+            </Grid>
+            <Grid item xs>
+              <TextField
+                multiline
+                fullWidth
+                value={admins.join("\n")}
+                onChange={(e) =>
+                  this.setState({ admins: e.target.value.split("\n") })
+                }
+              />
+            </Grid>
+            <Grid item xs>
+              <Typography>
+                <I18n>
+                  <En>Reviewers</En>
+                  <Fr>Réviseurs</Fr>
+                </I18n>
+              </Typography>
+            </Grid>
+            <Grid item xs>
+              <TextField
+                multiline
+                fullWidth
+                value={reviewers.join("\n")}
+                onChange={(e) =>
+                  this.setState({
+                    reviewers: e.target.value.split("\n"),
+                  })
+                }
+              />
+            </Grid>
+            <Grid item xs>
+              <Button
+                startIcon={<Save />}
+                variant="contained"
+                color="secondary"
+                onClick={() => this.save()}
+              >
+                <I18n>
+                  <En>Save</En>
+                  <Fr>Enregistrer</Fr>
+                </I18n>
+              </Button>
+            </Grid>
+          </>
+        )}
       </Grid>
-
-      {loading ? (
-        <CircularProgress />
-      ) : (
-        <>
-          <Grid item xs>
-            <Typography>
-              <I18n>
-                <En>Projects</En>
-                <Fr>Projets</Fr>
-              </I18n>
-            </Typography>
-          </Grid>
-          <Grid item xs>
-            <TextField
-              multiline
-              fullWidth
-              value={projects.join("\n")}
-              onChange={(e) =>
-                setState((prevState) => ({
-                  ...prevState,
-                  projects: e.target.value.split("\n"),
-                }))
-              }
-            />
-          </Grid>
-          <Grid item xs>
-            <Typography>
-              <I18n>
-                <En>Admins</En>
-                <Fr>Administrateurs</Fr>
-              </I18n>
-            </Typography>
-          </Grid>
-          <Grid item xs>
-            <TextField
-              multiline
-              fullWidth
-              value={admins.join("\n")}
-              onChange={(e) => 
-                setState(prevState => ({
-                  ...prevState,
-                  admins: e.target.value.split("\n"),
-                }))
-              }
-            />
-          </Grid>
-          <Grid item xs>
-            <Typography>
-              <I18n>
-                <En>Reviewers</En>
-                <Fr>Réviseurs</Fr>
-              </I18n>
-            </Typography>
-          </Grid>
-          <Grid item xs>
-            <TextField
-              multiline
-              fullWidth
-              value={reviewers.join("\n")}
-              onChange={(e) =>
-                setState(prevState => ({
-                ...prevState,
-                reviewers: e.target.value.split("\n"),
-                }))
-              }
-            />
-          </Grid>
-          <Grid item xs>
-        <Typography variant="h5">
-          <I18n>
-            <En>Enable DOI Creation</En>
-            <Fr>Activer la création de DOI</Fr>
-          </I18n>
-        </Typography>
-        <Typography>
-          <I18n>
-            <En>Enter the region's DataCite Prefix and Authentication Credentials</En>
-            <Fr>
-                Entrez le préfixe DataCite et les informations d'authentification de la région.
-            </Fr>
-          </I18n>
-        </Typography>
-      </Grid>
-          <Grid item xs>
-            <Typography>
-              <I18n>
-                <En>Select which Region/Organization</En>
-                <Fr>Sélectionnez quelle région/organisation</Fr>
-              </I18n>
-            </Typography>
-          </Grid>
-          {/* TO DO - ADD DROPDOWN LIST OF ALL AVAILABLE REGIONS AND ORGANIZATIONS */}
-          <Grid item xs>
-            <Typography>
-              <I18n>
-                <En>Datacite Prefix For example, '10.0000'</En>
-                <Fr>Préfixe Datacite par exemple, '10.0000'</Fr>
-              </I18n>
-            </Typography>
-          </Grid>
-          <Grid item xs>
-            <TextField
-              multiline
-              fullWidth
-              onChange={(e) =>
-              console.log(e.target.value)
-              }
-            />
-          </Grid>
-          <Grid item xs>
-            <Typography>
-              <I18n>
-                <En>Datacite account ID</En>
-                <Fr>Identifiant du compte Datacite</Fr>
-              </I18n>
-            </Typography>
-          </Grid>
-          <Grid item xs>
-            <TextField
-              multiline
-              fullWidth
-              onChange={(e) =>
-              console.log(e.target.value)
-              }
-            />
-          </Grid>
-          <Grid item xs>
-            <Typography>
-              <I18n>
-                <En>Datacite account Password</En>
-                <Fr>Mot de passe du compte Datacite</Fr>
-              </I18n>
-            </Typography>
-          </Grid>
-          <Grid item xs>
-            <TextField
-              multiline
-              fullWidth
-              onChange={(e) =>
-              console.log(e.target.value)
-              }
-            />
-          </Grid>
-          <Grid item xs>
-            <Button
-              startIcon={<Save />}
-              variant="contained"
-              color="secondary"
-              onClick={() => save()}
-            >
-              <I18n>
-                <En>Save</En>
-                <Fr>Enregistrer</Fr>
-              </I18n>
-            </Button>
-          </Grid>
-        </>
-      )}
-    </Grid>
-  );
+    );
+  }
 }
 
 export default Admin;
