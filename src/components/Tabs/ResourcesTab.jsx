c42c370bc31807f953e46f5d480c7c1eb0ecc72e
Matthew Foster <matthew.foster@hakai.org>
2024-02-22 09:52:00 -0800

    remove unneeded description text from resource tab

--- src/components/Tabs/ResourcesTab.jsx
+++ src/components/Tabs/ResourcesTab.jsx
@@ -136,14 +136,6 @@ const ResourcesTab = ({ disabled, record, updateRecord }) => {
                       <li>Metadata records on other catalogues such as OBIS or FRDR that describe the same dataset</li>
                       <li>Any work that adds context to or describes the primary resource for which you are creating this metadata record for</li>
                     </ul>
-
-                    Some of the ways a work can be related are:
-                    <ul>
-                      <li>Cross Reference - Reference from one record to another.	Use to identify related documents or related records that may be part of the same collection or project</li>
-                      <li>Dependency - Associated through a dependency</li>
-                      <li>Is Composed Of/Larger Work Citation - Reference to record(s) that are parts of this resource or which this one is a part of</li>
-                      <li>Revision Of - Resource is a revision of associated record</li>
-                    </ul>
                   </En>
                   <Fr>
                     Les œuvres connexes peuvent être :
@@ -151,14 +143,6 @@ const ResourcesTab = ({ disabled, record, updateRecord }) => {
                       <li>Autres ensembles de données faisant partie de la même collection, du même projet ou du même protocole d'échantillonnage</li>
                       <li>Enregistrements de métadonnées sur d'autres catalogues tels que OBIS ou FRDR qui décrivent le même ensemble de données</li>
                       <li>Tout travail qui ajoute du contexte ou décrit la ressource principale pour laquelle vous créez cet enregistrement de métadonnées</li>
-                    </ul>
-
-                    Certaines des façons dont une œuvre peut être liée sont :
-                    <ul>
-                      <li>Référence croisée - référence d'un enregistrement à un autre. À utiliser pour identifier des documents ou des enregistrements associés pouvant faire partie de la même collection ou du même projet.</li>
-                      <li>Dépendance - Associé via une dépendance</li>
-                      <li>Est composé de/Citation de travail plus grande - Référence à un ou plusieurs enregistrements qui font partie de cette ressource ou dont celle-ci fait partie</li>
-                      <li>Révision de - la ressource est une révision de l'enregistrement associé</li>
                     </ul>
                   </Fr>
                 </I18n>
