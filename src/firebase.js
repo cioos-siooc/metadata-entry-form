ef4fb7ed5b9950eccf3a4e1bd3ddc9ac9781a6f9
Austen Sorochak <austensorochak@gmail.com>
2024-03-26 15:07:41 -0700

    add config for dev project

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,7 @@ const deployedOnTestSserver = process.env.REACT_APP_DEV_DEPLOYMENT;
 const productionDB = "https://cioos-metadata-form.firebaseio.com";
 const devDB = "https://cioos-metadata-form-dev.firebaseio.com";
 
-const config = {
+const prodConfig = {
   apiKey: "AIzaSyAdlELZS5Lbea5NquotMT8amwO-Lc_7ogc",
   authDomain: "cioos-metadata-form.firebaseapp.com",
   databaseURL:
@@ -21,6 +21,20 @@ const config = {
   messagingSenderId: "646114203434",
   appId: "1:646114203434:web:bccceadc5144270f98f053",
 };
+
+const devConfig = {
+  apiKey: "AIzaSyCbwcwHh3sEIvD3gm8SBwCQfAyjGS9a0sc",
+  authDomain: "cioos-metadata-form-dev.firebaseapp.com",
+  databaseURL: "https://cioos-metadata-form-dev-default-rtdb.firebaseio.com",
+  projectId: "cioos-metadata-form-dev",
+  storageBucket: "cioos-metadata-form-dev.appspot.com",
+  messagingSenderId: "392401521083",
+  appId: "1:392401521083:web:45d1539f9d284f446d5c9e",
+}
+
+const config = process.env.REACT_APP_ENVIRONMENT === "production" && !deployedOnTestSserver
+  ? prodConfig
+  : devConfig;
 
 firebase.initializeApp(config);
 // uncomment below to use firebase emulator for local development
