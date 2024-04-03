69a31dce80e523a259dddf681bc8237d2dd94b08
Matthew Foster <matthew.foster@hakai.org>
2024-04-03 10:53:03 -0700

    add comment re api key and github secrets

--- src/firebase.js
+++ src/firebase.js
@@ -10,6 +10,13 @@ const productionDB = "https://cioos-metadata-form.firebaseio.com";
 const devDB = "https://cioos-metadata-form-dev.firebaseio.com";
 
 const config = {
+  // see https://console.cloud.google.com/apis/credentials?project=cioos-metadata-form
+  // and https://console.cloud.google.com/apis/credentials?project=cioos-metadata-form-dev
+  // for api key location which is then stored in a github secret and added to several
+  // github actions to support testing and deployment.
+  // see https://firebase.google.com/docs/projects/api-keys for a discussion of why we 
+  // don't need to restrict api keys for firebase but might in some situations.
+  // To prevent the future foot gun, we are restricting the key now.
   apiKey: process.env.REACT_APP_GOOGLE_CLOUD_API_KEY,
   authDomain: "cioos-metadata-form.firebaseapp.com",
   databaseURL:
