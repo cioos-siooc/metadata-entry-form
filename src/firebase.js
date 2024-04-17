c07f97e6170a17f57d0edbb05094ed0c5aaa932c
Matthew Foster <matthew.foster@hakai.org>
2024-04-17 10:04:40 -0700

    split prod and dev config

--- src/firebase.js
+++ src/firebase.js
@@ -7,10 +7,7 @@ import { initializeApp } from 'firebase/app'
 
 const deployedOnTestServer = process.env.REACT_APP_DEV_DEPLOYMENT;
 
-const productionDB = "https://cioos-metadata-form.firebaseio.com";
-const devDB = "https://cioos-metadata-form-dev.firebaseio.com";
-
-const config = {
+const prodConfig = {
   // see https://console.cloud.google.com/apis/credentials?project=cioos-metadata-form
   // and https://console.cloud.google.com/apis/credentials?project=cioos-metadata-form-dev
   // for api key location which is then stored in a github secret and added to several
@@ -18,23 +15,29 @@ const config = {
   // see https://firebase.google.com/docs/projects/api-keys for a discussion of why we 
   // don't need to restrict api keys for firebase but might in some situations.
   // To prevent the future foot gun, we are restricting the key now.
-  apiKey: process.env.NODE_ENV === "production" && !deployedOnTestServer 
-      ? process.env.REACT_APP_GOOGLE_CLOUD_API_KEY
-      : process.env.REACT_APP_GOOGLE_CLOUD_API_KEY_DEV,
-  authDomain: process.env.NODE_ENV === "production" && !deployedOnTestServer
-      ? "cioos-metadata-form.firebaseapp.com"
-      : "cioos-metadata-form-dev.firebaseapp.com",
-  databaseURL:
-    process.env.NODE_ENV === "production" && !deployedOnTestServer
-      ? productionDB
-      : devDB,
-  projectId: process.env.NODE_ENV === "production" && !deployedOnTestServer
-      ? "cioos-metadata-form"
-      : "cioos-metadata-form-dev",
+  apiKey: process.env.REACT_APP_GOOGLE_CLOUD_API_KEY,
+  authDomain: "cioos-metadata-form.firebaseapp.com",
+  databaseURL: "https://cioos-metadata-form.firebaseio.com",
+  projectId: "cioos-metadata-form",
   storageBucket: "cioos-metadata-form.appspot.com",
   messagingSenderId: "646114203434",
   appId: "1:646114203434:web:bccceadc5144270f98f053",
 };
+
+const devConfig = {
+  apiKey: process.env.REACT_APP_GOOGLE_CLOUD_API_KEY_DEV,
+  authDomain: "cioos-metadata-form-dev.firebaseapp.com",
+  databaseURL: "https://cioos-metadata-form-dev.firebaseio.com",
+  projectId: "cioos-metadata-form-dev",
+  storageBucket: "cioos-metadata-form-dev.appspot.com",
+  messagingSenderId: "392401521083",
+  appId: "1:392401521083:web:45d1539f9d284f446d5c9e",
+};
+
+
+const config = process.env.NODE_ENV === "production" && !deployedOnTestServer
+  ? prodConfig
+  : devConfig
 
 if (window.location.hostname === "localhost" && deployedOnTestServer) {
   config.databaseURL = "http://localhost:9001?ns=cioos-metadata-form"
