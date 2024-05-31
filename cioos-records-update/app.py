690a2f744510d6ef16964e0f354da9bc034b5894
Matthew Foster <matthew.foster@hakai.org>
2024-05-31 13:01:13 -0700

    convert key to json from string

--- cioos-records-update/app.py
+++ cioos-records-update/app.py
@@ -2,6 +2,7 @@ import glob
 import os
 import traceback
 from pathlib import Path
+import json
 
 import yaml
 from firebase_to_xml.__main__ import get_filename
@@ -23,7 +24,7 @@ sentry_sdk.init(
 
 # on the server its run inside docker, the values of xml, key.json work for the server
 firebase_auth_key_file = "key.json"
-firebase_auth_key_json = os.environ.get('FIREBASE_SERVICE_ACCOUNT_KEY')
+firebase_auth_key_json = json.loads(os.environ.get('FIREBASE_SERVICE_ACCOUNT_KEY','{}'))
 # this is bind mounted to /var/www/html/dev/metadata
 xml_folder = "xml"
 
