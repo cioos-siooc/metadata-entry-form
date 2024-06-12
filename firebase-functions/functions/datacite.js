3513417854a437841adcf37e1c7e009bfe4d7d6b
Matthew Foster <matthew.foster@hakai.org>
2024-06-12 09:31:13 -0700

    add documentation to functions

--- firebase-functions/functions/datacite.js
+++ firebase-functions/functions/datacite.js
@@ -4,6 +4,8 @@ const baseUrl = "https://api.datacite.org/dois/";
 const functions = require("firebase-functions");
 const axios = require("axios");
 
+// Use the existing firebase record (data) to create a draft doi on datacite. Datacite credentails 
+// are pulled from the admin section of the firebase db
 exports.createDraftDoi = functions.https.onCall(async (data) => {
 
   const { record, region } = data;
@@ -61,6 +63,8 @@ exports.createDraftDoi = functions.https.onCall(async (data) => {
   }
 });
 
+// Use the existing firebase record (dataObj) to update and existing draft doi on datacite. Datacite credentails 
+// are pulled from the admin section of the firebase db
 exports.updateDraftDoi = functions.https.onCall(async (dataObj) => {
   const { doi, region, data } = dataObj;
   let authHash
@@ -116,6 +120,8 @@ exports.updateDraftDoi = functions.https.onCall(async (dataObj) => {
   }
 });
 
+// Delete an existing draft doi on datacite tha matches doi saved in the firebase record (data). Datacite credentails 
+// are pulled from the admin section of the firebase db
 exports.deleteDraftDoi = functions.https.onCall(async (data) => {
 
   const { doi, region } = data;
@@ -165,6 +171,10 @@ exports.deleteDraftDoi = functions.https.onCall(async (data) => {
   }
 });
 
+// Get status of doi, this could be Draft, Registered, Findable, or Unknown. The status od Findable and Registered 
+// doi's can be determined by anyone while the status od draft doi's can only determined if they are part of the account
+// accessible using the saved datacite credentials in the admin section of the database. If the status can not be determined a
+// value of Unknown is returned
 exports.getDoiStatus = functions.https.onCall(async (data) => {
 
   let prefix;
@@ -227,6 +237,7 @@ exports.getDoiStatus = functions.https.onCall(async (data) => {
 
 });
 
+// helper function to get the datacite credentials from the database so they are not sent to the client
 exports.getCredentialsStored = functions.https.onCall(async (data) => {
   try {
     const credentialsRef = admin.database().ref('admin').child(data).child("dataciteCredentials");
@@ -244,6 +255,7 @@ exports.getCredentialsStored = functions.https.onCall(async (data) => {
   }
 });
 
+// helper function to get the datacite prefix from the database. this value is not special and can be sent to the client.
 exports.getDatacitePrefix = functions.https.onCall(async (region) => {
   try {
     const prefix = (await admin.database().ref('admin').child(region).child("dataciteCredentials").child("prefix").once("value")).val();
