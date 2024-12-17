6c3923369ebcc6cc2cc98fa4983bbf6614ea8165
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2024-12-17 14:00:23 -0500

    reformat cli section of code

--- firebase_to_xml/firebase_to_xml/__main__.py
+++ firebase_to_xml/firebase_to_xml/__main__.py
@@ -17,6 +17,8 @@ from metadata_xml.template_functions import metadata_to_xml
 from firebase_to_xml.record_json_to_yaml import record_json_to_yaml
 from tqdm import tqdm
 
+load_dotenv()
+
 
 def get_filename(record):
     """Creates a filename by combinig the title and UUID"""
@@ -29,19 +31,84 @@ def get_filename(record):
 
 
 @logger.catch
-def main():
+def main(
+    record_url,
+    also_save_yaml,
+    encoding,
+    xml_directory,
+    region,
+    status,
+    database_url,
+    key,
+):
     "Get arguments from command line and run script"
 
-    load_dotenv()
+    # get list of records from Firebase
+    record_list = get_records_from_firebase(
+        region=region,
+        firebase_auth_key_file=key,
+        record_url=record_url,
+        record_status=status.split(","),
+        database_url=database_url,
+    )
 
+    # translate each record to YAML and then to XML
+    for record in tqdm(record_list, desc=f"Processing {region} records"):
+        # if single record it uses std out, hide info
+        if not record_url:
+            logger.info(
+                "Processing",
+                f"'{record['title']['en']}'",
+                f"'{record['title']['fr']}'",
+                record["identifier"],
+                record["recordID"],
+                "\n",
+            )
+
+        try:
+            record_yaml = record_json_to_yaml(record)
+
+            organization = record.get("organization", "")
+            name = record.get("filename") or get_filename(record)
+
+            output_directory = Path(xml_directory) / organization
+            output_directory.mkdir(parents=True, exist_ok=True)
+
+            # output yaml
+            if also_save_yaml:
+                yaml_file = output_directory / f"{name}.yaml"
+                yaml_file.write_text(
+                    yaml.dump(record_yaml, allow_unicode=True, sort_keys=False),
+                    encoding=encoding,
+                )
+
+            # render xml template and write to file
+            xml = metadata_to_xml(record_yaml)
+            if record_url:
+                logger.info(xml)
+            else:
+                xml_file = output_directory / f"{name}.xml"
+                xml_file.write_text(xml, encoding=encoding)
+                logger.info("Wrote " + xml_file.name)
+
+        except Exception:
+            print(traceback.format_exc())
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Convert firebase metadata form to xml and optionaly yaml"
     )
 
     parser.add_argument("--key", required=True, help="Path to firebase OAuth2 key file")
-    parser.add_argument("--out", default=".", help="Folder to save xml")
     parser.add_argument(
-        "--yaml", action="store_true", help="Whether to output yaml file as well as xml"
+        "--xml-directory", "--out", default=".", help="Folder to save xml"
+    )
+    parser.add_argument(
+        "--also-save-yaml",
+        "--yaml",
+        action="store_true",
+        help="Whether to output yaml file as well as xml",
     )
     parser.add_argument("--region", required=True, help="Eg pacific/stlaurent/atlantic")
     parser.add_argument(
@@ -70,65 +137,4 @@ def main():
     )
     args = vars(parser.parse_args())
 
-    record_url = args["record_url"]
-    also_save_yaml = args["yaml"]
-    encoding = args["encoding"]
-    xml_directory = Path(args["out"])
-    region = args["region"]
-
-    # get list of records from Firebase
-    record_list = get_records_from_firebase(
-        region=args["region"],
-        firebase_auth_key_file=args["key"],
-        record_url=record_url,
-        record_status=args["status"].split(","),
-        database_url=args["database_url"],
-    )
-
-    # translate each record to YAML and then to XML
-    for record in tqdm(record_list, desc=f"Processing {region} records"):
-        # if single record it uses std out, hide info
-        if not record_url:
-            logger.info(
-                "Processing",
-                f"'{record['title']['en']}'",
-                f"'{record['title']['fr']}'",
-                record["identifier"],
-                record["recordID"],
-                "\n",
-            )
-
-        try:
-            record_yaml = record_json_to_yaml(record)
-
-            organization = record.get("organization", "")
-
-            name = record.get("filename") or get_filename(record)
-
-            output_directory = xml_directory / organization
-
-            output_directory.mkdir(parents=True, exist_ok=True)
-
-            # output yaml
-            if also_save_yaml:
-                yaml_file = output_directory / f"{name}.yaml"
-                yaml_file.write_text(
-                    yaml.dump(record_yaml, allow_unicode=True, sort_keys=False),
-                    encoding=encoding,
-                )
-
-            # render xml template and write to file
-            xml = metadata_to_xml(record_yaml)
-            if record_url:
-                logger.info(xml)
-            else:
-                xml_file = output_directory / f"{name}.xml"
-                xml_file.write_text(xml, encoding=encoding)
-                logger.info("Wrote " + xml_file.name)
-
-        except Exception:
-            print(traceback.format_exc())
-
-
-if __name__ == "__main__":
-    main()
+    main(**args)
