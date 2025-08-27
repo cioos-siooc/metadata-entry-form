923c20d6b4f4710bb9ea643a420b08b886308224
OGSL-BRYAN\turco <turcotteb@ogsl.ca>
2025-08-27 14:34:45 -0400

    removing the unused method

--- src/utils/firebaseRecordFunctions.js
+++ src/utils/firebaseRecordFunctions.js
@@ -197,10 +197,3 @@ export async function updateSharedRecord(userID, recordID, authorID, region, sha
       .catch(error => { throw new Error(`Error unsharing record by author ${authorID} with user ${userID}: ${error}`) });
   }
 }
-
-
-export async function pathExists(path) {
-  const database = getDatabase();
-  const snapshot = await get(ref(database, path));
-  return snapshot.exists();
-}
