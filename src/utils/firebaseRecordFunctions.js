f058e3870036a1a4cacf5acafa887cb459be52b3
Jessy Barrette <jessy.barrette@users.noreply.gitlab.com>
2026-01-07 15:26:54 -0500

    Fix incorrect string interpolation in transferRecord and returnRecordToDraft functions

--- src/utils/firebaseRecordFunctions.js
+++ src/utils/firebaseRecordFunctions.js
@@ -118,7 +118,7 @@ export async function transferRecord(
   if (userMatch) {
     const [matchingUserID] = userMatch;
 
-    const recordRef = child(regionUsersRef, `${sourceUserID})/records/${recordID}`);
+    const recordRef = child(regionUsersRef, `${sourceUserID}/records/${recordID}`);
 
     const record = (await get(recordRef, "value")).val();
 
@@ -137,7 +137,7 @@ export async function transferRecord(
 
 export function returnRecordToDraft(region, userID, key) {
   const database = getDatabase(firebase);
-  return set(ref(database, `${region})/users/${userID}/records/${key}/status`), "");
+  return set(ref(database, `${region}/users/${userID}/records/${key}/status`), "");
 }
 
 export async function getRegionProjects(region) {
