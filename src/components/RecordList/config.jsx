b4e450d19a016fb2a296dd911c163a87460f3f7d
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2026-02-05 03:57:24 +0000

    feat: enable download button in reviewer configuration

--- src/components/RecordList/config.jsx
+++ src/components/RecordList/config.jsx
@@ -76,7 +76,7 @@ export const reviewerConfig = {
     showUnPublishAction: true,
     showUnSubmitAction: true,
     showTransferButton: true,
-    showDownloadButton: false,
+    showDownloadButton: true,
     showGithubPublishAction: true,
   },
 
